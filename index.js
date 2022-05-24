910ea822e266f825f102de35cb75ac604f0e77b6
Dillon Beers <dr.johnson3@hotmail.com>
2022-05-24 02:06:12 -0600

    Meh

---
+++ index.js
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="UTF-8">
+            <title>Page Titles</title>
+        </head>
+
+        <body>
+            <script>
+                /* Our JavaScript don' goes' right here! Happy Face */
+
+                let x = "(4+5+6+7+4+2+1)";
+                console.log(x);
+                
+                let x = "(4+6+9) / 77";
+                alert(x);
+
+                let a = 10;
+                console.log(a);
+
+                let a = "9 * a";
+
+                let b = "7 * a";
+                console.log(b);
+
+                let max = 57;
+                
+                let actual = "max - 13";
+
+                let percentage = "actual / max";
+                console.log(percentage)
+
+            </script>
+        </body>
+    </html>
