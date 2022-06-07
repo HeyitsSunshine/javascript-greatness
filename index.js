87a9e709b5b19e12c12e00f209cad82faf5dd95c
Dillon Beers <dr.johnson3@hotmail.com>
2022-06-07 04:17:13 -0600

    Beasting some javascript, lol, more like, vice versa, sort of.

--- index.js
+++ index.js
@@ -9,27 +9,6 @@
             <script>
                 /* Our JavaScript don' goes' right here! Happy Face */
 
-                let x = "(4+5+6+7+4+2+1)";
-                console.log(x);
-                
-                let x = "(4+6+9) / 77";
-                alert(x);
-
-                let a = 10;
-                console.log(a);
-
-                let a = "9 * a";
-
-                let b = "7 * a";
-                console.log(b);
-
-                let max = 57;
-                
-                let actual = "max - 13";
-
-                let percentage = "actual / max";
-                console.log(percentage)
-
             </script>
         </body>
     </html>
